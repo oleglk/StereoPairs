68b1fc8bba7e91c09e408a72b76dd588b033ab77
oleglk <oleg.kosyakovsky@gmail.com>
2016-05-06 15:42:19 +0300

    Create dir for img_proc package

---
+++ StereoPairs_TCL/img_proc/img_proc.oklib.tcl
@@ -0,0 +1,5 @@
+# img_proc.oklib.tcl - img_proc package init
+
+set PKG_DIR $OK_TCLSRC_ROOT/img_proc
+puts "-- Building package img_proc --"
+source $OK_TCLSRC_ROOT/load_pkg.tcl
---
+++ StereoPairs_TCL/img_proc/pkgIndex.tcl
@@ -0,0 +1,1 @@
+package ifneeded img_proc 1.1 [list source [file join $dir img_proc.oklib.tcl]]
