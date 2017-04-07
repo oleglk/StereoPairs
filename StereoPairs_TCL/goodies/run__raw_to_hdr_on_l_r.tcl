f6c91a3eedee2e758a62f9f7e36417036579842b
oleglk <oleg.kosyakovsky@gmail.com>
2017-04-07 15:27:03 +0300

    Use raw2hdr with forced equal WB

--- StereoPairs_TCL/goodies/run__raw_to_hdr_on_l_r.tcl
+++ StereoPairs_TCL/goodies/run__raw_to_hdr_on_l_r.tcl
@@ -8,5 +8,5 @@ set SCRIPT_DIR [file dirname [info script]]
 source [file join $SCRIPT_DIR "raw_to_hdr.tcl"]
 
 # (3) Execute the main procedure of "raw_to_hdr.tcl" script
-raw_to_hdr_main "-inp_dirs {L R} -out_subdir_name OUT -final_depth 8 -raw_ext ARW -tools_paths_file d:/Work/StereoPairs/ext_tool_dirs.csv"
+raw_to_hdr_main "-inp_dirs {L R} -out_subdir_name OUT -final_depth 8 -raw_ext ARW -wb_out_file wb_dir1.csv -wb_inp_file wb_dir1.csv  -tools_paths_file d:/Work/StereoPairs/ext_tool_dirs.csv"
 ################################################################################
