fc7a18fc01f24be9f4fc15c5376bc572a97d2290
oleglk <oleg.kosyakovsky@gmail.com>
2017-04-08 11:38:05 +0300

    Included tkcon

--- StereoPairs_TCL/goodies/wrap__run_tcl_raw_to_hdr.bat
+++ StereoPairs_TCL/goodies/wrap__run_tcl_raw_to_hdr.bat
@@ -16,6 +16,8 @@ if ERRORLEVEL 1 (
 
 @REM If the command preceded by "start", the DOS and wish windows aren't closed when finished
 cmd /K ""C:\Program Files (x86)\etcl\bin\etcl.exe" "%SCRIPT_DIR%\run__raw_to_hdr_on_l_r.tcl""
+@REM The below attempt truncates long script path into 8.3 before passing to TCL
+@REM cmd /K ""C:\Program Files (x86)\etcl\bin\etcl.exe" "%SCRIPT_DIR%\..\..\Libs_TCL\tkcon.tcl" -eval "%SCRIPT_DIR%\run__raw_to_hdr_on_l_r.tcl""
 
 @echo on
 @exit /B 0
