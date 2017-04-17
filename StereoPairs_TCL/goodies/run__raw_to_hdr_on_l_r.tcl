820e1ee454a498ee2c28032923049f962005cd55
oleglk <oleg.kosyakovsky@gmail.com>
2017-04-17 11:18:34 +0300

    Fix path of tool-paths file

--- StereoPairs_TCL/goodies/run__raw_to_hdr_on_l_r.tcl
+++ StereoPairs_TCL/goodies/run__raw_to_hdr_on_l_r.tcl
@@ -1,13 +1,13 @@
 # run__raw_to_hdr_on_l_r.tcl - a "sourceable" file that runs raw_to_hdr.tcl on L/ and R/ subdirectories
 
 ################################################################################
-# (1) Detect "raw_to_hdr.tcl" script location
-set SCRIPT_DIR [file dirname [info script]]
+# (1) Detect "raw_to_hdr.tcl" script location (use surely unique variable name)
+set SCRIPT_DIR__raw_to_hdr [file dirname [info script]]
 
 # (2) Load the code from "raw_to_hdr.tcl" script
-source [file join $SCRIPT_DIR "raw_to_hdr.tcl"]
+source [file join $SCRIPT_DIR__raw_to_hdr "raw_to_hdr.tcl"]
 
 # (3) Execute the main procedure of "raw_to_hdr.tcl" script
 # (location of tool-path file reflects Dualcam-Companion software structure)
-raw_to_hdr_main "-inp_dirs {L R} -out_subdir_name OUT -final_depth 8 -raw_ext ARW -wb_out_file wb_dir1.csv -wb_inp_file wb_dir1.csv  -tools_paths_file [file join $SCRIPT_DIR ".." ".." ext_tool_dirs.csv]"
+raw_to_hdr_main "-inp_dirs {L R} -out_subdir_name OUT -final_depth 8 -raw_ext ARW -wb_out_file wb_dir1.csv -wb_inp_file wb_dir1.csv  -tools_paths_file [file join $SCRIPT_DIR__raw_to_hdr ".." ".." ext_tool_dirs.csv]"
 ################################################################################
