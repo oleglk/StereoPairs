387ec578134d19367a5392afd5bf2129c7eacaca
oleglk <oleg.kosyakovsky@gmail.com>
2017-04-30 22:31:31 +0300

    Read naming parameters for RAW conversion from the preferences file

--- StereoPairs_TCL/goodies/run__raw_to_hdr_on_l_r.tcl
+++ StereoPairs_TCL/goodies/run__raw_to_hdr_on_l_r.tcl
@@ -48,6 +48,40 @@ proc _swap_lr_names_in_csv_file {inPath outPath namePos descr}  {
   ok_info_msg "Wrote $cntSwapped renamed records into $descr file '$outPath'"
   return  1
 }
+
+
+# Reads and applies relevant preferences from DualCam-Companion
+proc _load_some_preferences {} {
+  array unset ::STS ;   # array for global settings ;  unset once per a project
+  preferences_set_initial_values  ; # initializing the settings is mandatory
+  # load default settings if possible
+  set allLoaded 1;  # as if all succeeded
+  if { 0 == [preferences_read_and_apply] }  {
+    ok_warn_msg "Preferences were not loaded; will use hardcoded values"
+    return  0
+  } else {
+    # perform initializations dependent on the saved preferences
+    if { 0 == [preferences_get_val -name_format_left specLeft]} {
+      ok_err_msg "Missing preference for left image name-format"
+      set allLoaded 0
+    }
+    if { 0 == [preferences_get_val -name_format_right specRight]} {
+      ok_err_msg "Missing preference for right image name-format"
+      set allLoaded 0
+    }
+    if { 0 == [set_naming_parameters_from_left_right_specs \
+                                          $specLeft $specRight] } {
+      set allLoaded 0;  # error already printed
+    }
+  }
+  if { $allLoaded == 1 }  {
+    ok_info_msg "Preferences successfully loaded and applied"
+    return  1
+  } else {
+    ok_warn_msg "Preferences were not applied; will use hardcoded values"
+    return  0
+  }
+}
 ################################################################################
 ## End of local procedures
 ################################################################################
@@ -68,9 +102,10 @@ set SCRIPT_DIR__raw_to_hdr [file dirname [info script]]
 # (2) Load the code from "raw_to_hdr.tcl" script;  imports library utilities too
 source [file join $SCRIPT_DIR__raw_to_hdr "raw_to_hdr.tcl"]
 source [file join $SCRIPT_DIR__raw_to_hdr ".." "stereopair_naming.tcl"]
+source [file join $SCRIPT_DIR__raw_to_hdr ".." "preferences_mgr.tcl"]
 
 # (3)
-if { 0 == [array exists NAMING] }  {  ; # unless defined by environment
+if { 0 == [_load_some_preferences] }  {  ; # unless defined by preferences
   _set_naming_parameters ""  ""  "-"  "_l"  "_r";   # for StereoPhotoMaker
 }
 
