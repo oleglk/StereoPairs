9c088d6bf37358bdce0fbe3d2dfc485c9d8c0b01
oleglk <oleg.kosyakovsky@gmail.com>
2018-01-19 17:55:25 +0200

    ok_try_get_free_disk_space_kb ok_get_filelist_disk_space

--- StereoPairs_TCL/ok_utils/disk_info.tcl
+++ StereoPairs_TCL/ok_utils/disk_info.tcl
@@ -3,12 +3,14 @@
 namespace eval ::ok_utils:: {
 
   namespace export                \
-    ok_df_k                 
+    ok_get_free_disk_space_kb     \
+    ok_try_get_free_disk_space_kb \
+    ok_get_filelist_disk_space
 }
 
 # Copied from "proc df-k" at http://wiki.tcl.tk/526#pagetoc071ae01c
 # Returns free disk space in kilobytes
-proc ::ok_utils::ok_df_k {{dir .}} {
+proc ::ok_utils::ok_get_free_disk_space_kb {{dir .}} {
     switch $::tcl_platform(os) {
     FreeBSD -
     Linux -
@@ -41,3 +43,37 @@ proc ::ok_utils::ok_df_k {{dir .}} {
     default {error "don't know how to df-k on $::tcl_platform(os)"}
     }
 } ;#RS
+
+
+# If possible, returns free disk space on the disk of path '$dir'.
+# On error returns -1
+proc ::ok_utils::ok_try_get_free_disk_space_kb {{dir .}} {
+  set tclExecResult [catch {set n [ok_get_free_disk_space_kb $dir]} execResult]
+  if { $tclExecResult != 0 } {
+    ok_err_msg "$execResult!"
+    ok_err_msg "Cannot check free space for '$dir'."
+    return  -1
+  }
+  return $n
+}
+
+
+# Calculates and returns total disk space consumed by files in 'filePathsList'.
+# On error returns -1 * <number-of-unreadable-files>
+proc ::ok_utils::ok_get_filelist_disk_space {filePathsList {priErr 1}}  {
+  set size 0;    set noaccess [list]
+  foreach filePath $filePathsList {
+    if { [file exists $filePath] && [file readable $filePath] } {
+      incr size [file size $filePath]
+    } else {
+      lappend noaccess $filePath
+    }
+  }
+  if { 0 != [llength $noaccess] }  {
+    if { $priErr }  {
+      ok_err_msg "Measuring used disk space encountered [llength $noaccess] inexistent and/or unreadable file(s)"
+    }
+    return  [expr -1 * [llength $noaccess]]
+  }
+  return  $size
+}
