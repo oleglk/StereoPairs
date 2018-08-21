38176b3b64cc15c5710c456a5cf3a0bdcd6fb4b5
oleglk <oleg.kosyakovsky@gmail.com>
2018-08-21 23:00:52 +0300

    Remove a space

--- StereoPairs_TCL/goodies/run__preview_raw_to_hdr_rotate_and_crop.tcl
+++ StereoPairs_TCL/goodies/run__preview_raw_to_hdr_rotate_and_crop.tcl
@@ -32,7 +32,7 @@ if { 1 == [source [file join $SCRIPT_DIR__raw_to_hdr_rotate_and_crop "run__raw_t
 }
 
 # (5) Cleanup - remove DUALCAM_RAW2HDR_PREVIEW environment variable
-unset env(DUALCAM_RAW2HDR_PREVIEW) 
+unset env(DUALCAM_RAW2HDR_PREVIEW)
 ################################################################################
 
 return  $res
