64be0e0b0ba78c1eb3eeb4b62a38ee04a3b6921f
Oleg Kosyakovsky <Oleg.Kosyakovsky@gmail.com>
2018-10-23 18:04:50 +0300

    Started module for treating files in name-order

---
+++ StereoPairs_TCL/ok_utils/file_nameorder.tcl
@@ -0,0 +1,21 @@
+# file_nameorder.tcl -  making creation order match name order
+
+namespace eval ::ok_utils:: {
+
+  namespace export                \
+    ok_list_files_in_nameorder    \
+    ok_find_pace_in_nameorder
+}
+
+
+# Returns list of filenames in 'dirPath' in lexicographic name order.
+# On error returns "ERROR"
+proc ::ok_utils::ok_list_files_in_nameorder {dirPath} {
+  # TODO
+}
+
+
+# Returns index fot 'name' if it was inserted into 'nameList'
+proc ::ok_utils::ok_find_pace_in_nameorder {name nameList} {
+  # TODO
+}
